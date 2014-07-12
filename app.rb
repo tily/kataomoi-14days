13dfb669ff593d51ee5ac9eb2deb283db4382702
tily <tily05@gmail.com>
2014-07-12 15:31:06 +0900

    delete maintenance text

--- app.rb
+++ app.rb
@@ -63,7 +63,6 @@ __END__
 		%div.container
 			%div.jumbotron
 				%h1 14 日間の片想い
-				%p.bg-danger 前略 適当にメンテナンスします、なるべく手紙が消えないようにいたします
 				%a{href:'/'} トップ
 				&nbsp;|&nbsp;
 				%a{href:'/letters/write'} 手紙をおくる
